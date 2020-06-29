50c35307f56e52f6a9fd8023c1ee0f1716b46421
Brian Gordon <gorbrin@amazon.com>
2020-06-29 16:03:31 +0300

    Added arguments support to code. Updated documentation with instructions

--- CODE/runme.py
+++ CODE/runme.py
@@ -4,6 +4,22 @@ from matting import video_matting
 from tracking import track_video
 import cv2
 import logging
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Please pass NO arguments for a full run (or -all flag), or select relevant flags to run specified parts of project.")
+parser.add_argument('-all', action='store_true', help='Pass this flag to run entire project process')
+parser.add_argument('-video_stab', action='store_true', help='Pass this flag to run Video Stab. part')
+parser.add_argument('-bs', action='store_true', help='Pass this flag to run Background Sub. part')
+parser.add_argument('-matting', action='store_true', help='Pass this flag to run Matting part')
+parser.add_argument('-tracking', action='store_true', help='Pass this flag to run Tracking part')
+
+args = parser.parse_args()
+RUN_ALL = False
+if args.all or len(sys.argv) == 1:
+    RUN_ALL = True
 
 LOG_FILENAME = '../Outputs/RunTimeLog.txt'
 logging.basicConfig(format='%(asctime)s %(message)s',
@@ -12,7 +28,11 @@ logging.basicConfig(format='%(asctime)s %(message)s',
                     filemode='w',
                     level=logging.INFO)
 
-stabilize_video('../Input/INPUT.avi')
-background_subtraction('../Outputs/stabilize.avi')
-video_matting('../Outputs/stabilize.avi','../Outputs/binary.avi',cv2.imread('../Input/background.jpg'))
-track_video('../Outputs/matted.avi')
+if RUN_ALL or args.video_stab:
+    stabilize_video('../Input/INPUT.avi')
+if RUN_ALL or args.bs:
+    background_subtraction('../Outputs/stabilize.avi')
+if RUN_ALL or args.matting:
+    video_matting('../Outputs/stabilize.avi', '../Outputs/binary.avi', cv2.imread('../Input/background.jpg'))
+if RUN_ALL or args.tracking:
+    track_video('../Outputs/matted.avi')
